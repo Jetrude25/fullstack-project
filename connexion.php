03f5f1b5286321aecfb2c53dd3f6616cea717f0a
Jetrude25 <gertrudehounake@gmail.com>
2022-09-23 10:09:51 +0000

    hebergement

--- connexion.php
+++ connexion.php
@@ -1,32 +1,4 @@
 <?php
-// $conn = mysqli_connect('localhost', 'root', '', 'portfolio') or die('connection failed');
-
-// if(isset($_POST['envoyer'])){
-//     $nom = mysqli_real_escape_string($conn, $_POST['nom']);
-//     $numero = mysqli_real_escape_string($conn, $_POST['numero']);
-//     $email = mysqli_real_escape_string($conn, $_POST['email']);
-//     $msg = mysqli_real_escape_string($conn, $_POST['message']);
-
-//     $select_message = mysqli_query($conn, "SELECT * FROM `message` WHERE name = '$nom' AND email = '$email' AND numero = '$numero' AND message ='$msg'") or die('query failed');
-
-//     if(mysqli_num_rows($select_message)> 0){
-//         $message[] = 'message envoyé';
-//      } else{ 
-//         mysqli_query($conn, "INSERT INTO `message`(nom,email,numero,message) VALUES('$name','email','numero','$msg')") or die('query failed');
-//         $message[] = 'Envoi reussit';
-//     }
-//     if(isset($message)){
-//         foreach($$message as $$message){
-//             echo '';
-           
-//         }
-//     }
-// }
-
-
-
-// echo "Record saved";
-
 
 $servername = "localhost";
 $username = "root";
@@ -45,7 +17,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO message (nom,numero,email,messages) VALUES ('$nom','$email','$numero', '$message')";
+$sql = "INSERT INTO message (nom,numero,email,messages) VALUES ('$nom','$numero';'$email', '$message')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
